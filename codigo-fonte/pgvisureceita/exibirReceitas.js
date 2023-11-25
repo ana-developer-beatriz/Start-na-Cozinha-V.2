406464fad805ecf81ff7fa758e5223e38aa8f0c5
ana-developer-beatriz <anabeatrizgithub@gmail.com>
2023-11-25 16:17:54 -0300

    Ajuste na pagHome - Adicionar titulo da receita e redirecionamento para receitas especificas.

--- codigo-fonte/pgvisureceita/exibirReceitas.js
+++ codigo-fonte/pgvisureceita/exibirReceitas.js
@@ -1,3 +1,4 @@
+
 function carregarReceitas(callback) {
     fetch('receitas.json')
         .then(function (response) {
@@ -20,60 +21,41 @@ function exibirIngredientes(receitaIndex) {
             itemLista.textContent = ingrediente;
             listaIngredientes.appendChild(itemLista);
         });
-        /*encontra o primeiro elemento no documento HTML com a classe list_ingr
-        e armazena na variável divIngredientes*/
+
         var divIngredientes = document.querySelector('.list_ingr');
-        /*Adiciona a lista de ingredientes criada dinamicamente (<ol>) à <div> com a classe list_ingr. Isso completa a construção da lista de 
-        ingredientes e a adiciona ao HTML. */
         divIngredientes.appendChild(listaIngredientes);
     });
 }
+
 function exibirModoDePreparo(receitaIndex) {
     carregarReceitas(function (receitas) {
         var modoDePreparo = receitas[receitaIndex].modo_de_preparo;
         
-        modoParagrafo = document.createElement('p');
-            modoParagrafo.textContent = modoDePreparo;
+        var modoParagrafo = document.createElement('p');
+        modoParagrafo.textContent = modoDePreparo;
+
         var containerReceitas = document.querySelector('.mdpreparo');
         containerReceitas.appendChild(modoParagrafo);
-       
     });
 }
+
 function exibirTituloDaReceita(receitaIndex) {
     carregarReceitas(function (receitas) {
         var titulo = receitas[receitaIndex].nome;
         
-        tituloDaReceitaH1 = document.createElement('h1');
-        tituloDaReceitaH1.textContent =  titulo;
+        var tituloDaReceitaH1 = document.createElement('h1');
+        tituloDaReceitaH1.textContent = titulo;
 
         var containerTitulo = document.querySelector('.tituloReceita');
         containerTitulo.appendChild(tituloDaReceitaH1);
-
-       
     });
 }
-function exibirTituloDaReceita(receitaIndex) {
-    carregarReceitas(function (receitas) {
-        var titulo = receitas[receitaIndex].nome;
-        
-        tituloDaReceitaH1 = document.createElement('h1');
-        tituloDaReceitaH1.textContent =  titulo;
-        console.log(tituloDaReceitaH1);
 
-        var containerTitulo = document.querySelector('.tituloReceita');
-        containerTitulo.appendChild(tituloDaReceitaH1);
-
-       
-    });
-}
 function tempoDePreparo(receitaIndex) {
     carregarReceitas(function (receitas) {
-
         const containerDetalhes = document.querySelector('.inforec');
-        // Lista para os detalhes
         const ulDetalhes = document.createElement('ul');
 
-        // Tempo de Preparo
         const liTempoPreparo = document.createElement('li');
         const imgTempoPreparo = document.createElement('img');
         imgTempoPreparo.src = receitas[receitaIndex].imagemTimer;
@@ -83,20 +65,15 @@ function tempoDePreparo(receitaIndex) {
         liTempoPreparo.appendChild(spanTempoPreparo);
         ulDetalhes.appendChild(liTempoPreparo);
 
-        // Nível de Dificuldade
         const liNivelDificuldade = document.createElement('li');
         const imgElementNivelDificuldade = document.createElement('img');
-            imgElementNivelDificuldade.src = receitas[receitaIndex].imagemFry;
+        imgElementNivelDificuldade.src = receitas[receitaIndex].imagemFry;
         const spanElementNivelDificuldade = document.createElement('span');
-            spanElementNivelDificuldade.textContent = receitas[receitaIndex].Nivel_de_dificuldade;
-            
-            liNivelDificuldade.appendChild(imgElementNivelDificuldade);
-            liNivelDificuldade.appendChild(spanElementNivelDificuldade);
-            ulDetalhes.appendChild(liNivelDificuldade);
+        spanElementNivelDificuldade.textContent = receitas[receitaIndex].Nivel_de_dificuldade;
+        liNivelDificuldade.appendChild(imgElementNivelDificuldade);
+        liNivelDificuldade.appendChild(spanElementNivelDificuldade);
+        ulDetalhes.appendChild(liNivelDificuldade);
 
-            containerDetalhes.appendChild(ulDetalhes);
-
-        // Chef
         const liChef = document.createElement('li');
         const imgChef = document.createElement('img');
         imgChef.src = receitas[receitaIndex].imagemChef;
@@ -105,54 +82,53 @@ function tempoDePreparo(receitaIndex) {
         liChef.appendChild(imgChef);
         liChef.appendChild(spanChef);
         ulDetalhes.appendChild(liChef);
+
+        containerDetalhes.appendChild(ulDetalhes);
     });
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const receitaIndex = urlParams.get('index');
 
-
-window.exibirIngredientes = exibirIngredientes;
-window.exibirModoDePreparo = exibirModoDePreparo;
-window.exibirTituloDaReceita = exibirTituloDaReceita;
-window.tempoDePreparo = tempoDePreparo;
- // Chama a função para exibir os ingredientes da primeira receita (índice 0)
-
-
- //Cadastrar um comentário
+    if (receitaIndex !== null) {
+        carregarReceitas(function (receitas) {
+            exibirTituloDaReceita(receitaIndex);
+            exibirIngredientes(receitaIndex);
+            exibirModoDePreparo(receitaIndex);
+            tempoDePreparo(receitaIndex);
+        });
+    } else {
+        console.error('Índice da receita não encontrado na URL.');
+    }
+});
 
 function enviarComent() {
-
     let areaComent = document.querySelector("#areaComent")
-
     let listComent = JSON.parse(localStorage.getItem("listComent") || "[]")
 
-    listComent.push(
-        {
-            comentario: areaComent.value
-        }
-    )
-    
-localStorage.setItem("listComent", JSON.stringify(listComent))
+    listComent.push({
+        comentario: areaComent.value
+    })
 
-exibirComents();
+    localStorage.setItem("listComent", JSON.stringify(listComent))
+    exibirComents();
 }
 
-function exibirComents(){
+function exibirComents() {
     let areaComent = JSON.parse(localStorage.getItem("listComent"));
     console.log('areaComent', areaComent);
 
     let allComents = document.querySelector(".allComents");
     console.log(allComents);
 
-    // Limpa o conteúdo 
     allComents.innerHTML = "";
 
-    // Percorre todos os comentários e cria elementos <li> para cada um
     areaComent.forEach((item) => {
         var itemLista = document.createElement('li');
         itemLista.textContent = item.comentario;
         allComents.appendChild(itemLista);
     });
-
 }
-exibirComents()
 
+exibirComents();
