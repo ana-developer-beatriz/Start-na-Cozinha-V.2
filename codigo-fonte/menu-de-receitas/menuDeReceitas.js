5116e2d882a904e186d196210229afe04086fd16
ana-developer-beatriz <anabeatrizgithub@gmail.com>
2023-12-08 12:30:42 -0300

    add functionality to search and select recipes

---
+++ codigo-fonte/menu-de-receitas/menuDeReceitas.js
@@ -0,0 +1,255 @@
+const recipesData = {
+  "receitas": [
+    {
+      "nome": "Lasanha à Bolonhesa",
+      "modo_de_preparo": "1. Cozinhe a massa da lasanha em água fervente com sal. \n2. Prepare o molho à bolonhesa com carne moída, molho de tomate, cebola, alho e temperos. \n3. Em um refratário, monte camadas de massa, molho e queijo. \n4. Repita as camadas até terminarem os ingredientes. \n5. Leve ao forno por 30 minutos ou até gratinar.",
+      "ingredientes": [
+        "500g de massa para lasanha",
+        "400g de carne moída",
+        "500ml de molho de tomate",
+        "1 cebola picada",
+        "2 dentes de alho picados",
+        "250g de queijo mussarela",
+        "Sal e pimenta a gosto",
+        "Orégano a gosto"
+      ],
+      "categorias": ["Massas"],
+      "Nome_Chef": "Ana Beatriz",
+      "imagemChef": "../img/icon_chef.png", 
+      "tempo_de_preparo": "30 minutos",
+      "imagemTimer": "../img/icon_timer.png",
+      "Nivel_de_dificuldade": "Médio",
+      "imagemFry": "../img/icon_fryingpan.png"   
+    },
+    {
+      "nome": "Salmão Grelhado",
+      "modo_de_preparo": "1. Tempere os filés de salmão com sal, pimenta e limão. \n2. Aqueça a grelha ou frigideira. \n3. Grelhe o salmão por 4-5 minutos de cada lado. \n4. Sirva com acompanhamentos a gosto.",
+      "ingredientes": [
+        "4 filés de salmão",
+        "Sal e pimenta a gosto",
+        "Suco de limão",
+        "Azeite"
+      ],
+      "categorias": ["Peixes"],
+      "tempo_de_preparo":"10 minutos",
+      "imagemTimer": "../img/icon_timer.png",
+      "Nivel_de_dificuldade":"Fácil",
+      "imagemFry": "../img/icon_fryingpan.png",
+      "Nome_Chef": "Isabella Smith",
+      "imagemChef": "../img/icon_chef.png"
+    },
+    {
+      "nome": "Bolo de Morango",
+      "modo_de_preparo": "1 - Bata na batedeira as claras em neve, adicione as gemas uma a uma e continue batendo, junte o açúcar e bata mais um pouco. \n2 - Junte a farinha peneirada e o leite bem quente e mexa bem, sem bater, por último coloque o fermento. \n3 - Leve para assar em forma redonda sem buraco no meio, untada e enfarinhada em forno pré-aquecido em temperatura média por no máximo 30 minutos ou quando tiver dourado e bem sequinho. \n3- Cresce bastante e fica bem fofinho.",
+      "ingredientes": [
+        "5 ovos",
+        "2 xícaras (chá) de farinha de trigo",
+        "1 colher (sopa) de fermento em pó",
+        "2 xícaras (chá) de açúcar",
+        "1 xícara (cha) de leite quente"
+      ],
+      "categorias": ["Bolos"],
+      "Nome_Chef": "Ana Beatriz",
+      "imagemChef": "../img/icon_chef.png",
+      "tempo_de_preparo": "1 hora e 30 minutos",
+      "imagemTimer": "../img/icon_timer.png",
+      "Nivel_de_dificuldade": "Alta",
+      "imagemFry": "../img/icon_fryingpan.png"
+    },
+    {
+      "nome": "Arroz com Legumes",
+      "modo_de_preparo": "1 – Refogue o alho e a cebola no azeite \n2 – Coloque o arroz e deixe fritar por cerca de 30 segundos \n3 – Adicione a água fervente juntamente com os legumes e o sal \n4 – Abaixe o fogo e deixe cozinhar até a água ter quase secado \n5 - Tampe a panela e aguarde cerca de 20 minutos antes de servir Se desejar fazer mais, é só seguir as proporções, principalmente da água",
+      "ingredientes": [
+        "2 xícaras Arroz Novato lavado",
+        "4 xícaras água fervente",
+        "2 dentes alho amassado",
+        "1/2 cebola picada",
+        "150g ervilha",
+        "150g milho",
+        "150g vagem picada",
+        "150g cenoura picada",
+        "Azeite",
+        "Sal a gosto"
+      ],
+      "categorias": ["Arroz"],
+      "Nome_Chef": "Ana Beatriz",
+      "imagemChef": "../img/icon_chef.png",
+      "tempo_de_preparo": "15 minutos",
+      "imagemTimer": "../img/icon_timer.png",
+      "Nivel_de_dificuldade": "Baixa",
+      "imagemFry": "../img/icon_fryingpan.png"
+    },
+    {
+      "nome": "Batata Doce",
+      "modo_de_preparo": "1. Lave, seque e descasque as batatas-doces. Corte em meias-luas grossas e transfira para uma panela média. Cubra com água e tempere com ½ colher (chá) de sal. \n2. Leve ao fogo alto e, assim que ferver, deixe cozinhar por cerca de 20 minutos, ou até ficarem macias - para verificar, espete com um garfo. \n3. Desligue o fogo e reserve 1 xícara (chá) da água do cozimento. Escorra as batatas por uma peneira. \n4. Volte as batatas, ainda quentes, para a mesma panela e bata com um mixer até ficar bem liso. Acrescente, aos poucos, a água do cozimento para dar o ponto. Se preferir um purê mais rústico, passe as batatas por um espremedor. \n5. Junte a manteiga e mexa bem com uma espátula até derreter e o purê ficar cremoso. Prove e, se necessário, tempere com mais sal. Sirva a seguir.",
+      "ingredientes": [
+        "3 batatas-doces brancas",
+        "limao",
+        "1 xícara (chá) da água do cozimento",
+        "2 colheres (sopa) de manteiga gelada",
+        "sal a gosto"
+      ],
+      "categorias": ["Batata"],
+      "Nome_Chef": "Ana Beatriz",
+      "imagemChef": "../img/icon_chef.png",
+      "tempo_de_preparo": "1 hora",
+      "imagemTimer": "../img/icon_timer.png",
+      "Nivel_de_dificuldade": "Média",
+      "imagemFry": "../img/icon_fryingpan.png"
+    },
+    {
+      "nome": "Feijão Tropeiro",
+      "modo_de_preparo": "1. Em uma panela grande (28cm de diâmetro), aqueça 1 colher de sopa de azeite, frite o bacon e reserve. Em seguida, frite e reserve os gomos de linguiça calabresa. \n2. Na mesma panela, frite a cebola e o alho e, depois, junte-os ao feijão escorrido. \n3. Junte novamente o bacon e a linguiça, e adicione a farinha de mandioca e os ovos mexidos. \n4. Acrescente a salsa e a cebolinha picadas. \n5. Tempere com o tempero em pó Knorr Meu Feijão. Prontinho, já pode servir!",
+      "ingredientes": [
+      "2 colheres de sopa de azeite",
+      "1 xícara de chá de bacon picado",
+      "1 xícara de chá de linguiça calabresa de porco fatiada",
+      "1 cebola picada",
+      "3 dentes de alho amassados",
+      "2 xícaras de chá de feijão carioca cozido e escorrido",
+      "1 1/2 colher de sopa de farinha de mandioca",
+      "2 ovos mexidos",
+      "1/2 xícara de chá de cheiro-verde (salsa e cebolinha)",
+      "1 sachê de Tempero em Pó"
+      ],
+      "categorias": ["Feijao"],
+      "Nome_Chef": "Ana Beatriz",
+      "imagemChef": "../img/icon_chef.png",
+      "tempo_de_preparo": "20 minutos",
+      "imagemTimer": "../img/icon_timer.png",
+      "Nivel_de_dificuldade": "Média",
+      "imagemFry": "../img/icon_fryingpan.png"
+    }
+  ]
+};
+
+  // function handleCategoryChange() {
+  //   searchRecipes();
+  // }
+  function closeModal() {
+    const openModals = document.querySelectorAll('dialog[open]');
+    openModals.forEach(modal => modal.close());
+  }
+
+function searchRecipes() {
+      const searchInput = document.getElementById('searchInput').value.toLowerCase();
+      const categoria1 = document.getElementById('categoria1').value.toLowerCase();
+      const categoria2 = document.getElementById('categoria2').value.toLowerCase();
+      const categoria3 = document.getElementById('categoria3').value.toLowerCase();
+      const categoria4 = document.getElementById('categoria4').value.toLowerCase();
+      const resultsContainer = document.getElementById('results');
+      resultsContainer.innerHTML = '';
+
+      const storedData = localStorage.getItem('recipeData');
+      const recipesData = storedData ? JSON.parse(storedData) : { receitas: [] };
+
+      const filteredRecipes = recipesData.receitas.filter(recipe => {
+        return (
+          (recipe.nome && recipe.nome.toLowerCase().includes(searchInput)) ||
+          (recipe.ingredientes && recipe.ingredientes.some(ingredient => ingredient && ingredient.toLowerCase().includes(searchInput))) ||
+          (recipe.Nome_Chef && recipe.Nome_Chef.toLowerCase().includes(searchInput)) ||
+          (categoria1 !== 'todos' && recipe.categorias && recipe.categorias.includes(categoria1)) ||
+          (categoria2 !== 'todos' && recipe.categorias && recipe.categorias.includes(categoria2)) ||
+          (categoria3 !== 'todos' && recipe.categorias && recipe.categorias.includes(categoria3)) ||
+          (categoria4 !== 'todos' && recipe.categorias && recipe.categorias.includes(categoria4))
+        );
+      });
+
+      if (filteredRecipes.length > 0) {
+        filteredRecipes.forEach(recipe => {
+          const recipeLink = document.createElement('a');
+          recipeLink.href = '#';
+          recipeLink.textContent = recipe.nome;
+          recipeLink.onclick = function () {
+            displayRecipeDetails(recipe);
+            return false;
+          };
+
+          resultsContainer.appendChild(recipeLink);
+          resultsContainer.appendChild(document.createElement('br'));
+        });
+      } else {
+        resultsContainer.innerHTML = '<p>Nenhuma receita encontrada.</p>';
+      }
+    }
+function displayRecipeDetails(recipe) {
+      const recipeElement = document.createElement('dialog');
+      recipeElement.innerHTML = `<h2>${recipe.nome}</h2>
+                                 <p>Chef: ${recipe.Nome_Chef}</p>
+                                 <p>Ingredientes: ${recipe.ingredientes ? recipe.ingredientes.join(', ') : 'N/A'}</p>
+                                 <p>Dificuldade: ${recipe.Nivel_de_dificuldade}</p>
+                                 <p>Tempo de preparação: ${recipe.tempo_de_preparo}</p>
+                                 <button onclick="closeModal()">Voltar</button>
+                                 <hr>`;
+      document.body.appendChild(recipeElement);
+      recipeElement.showModal();
+
+      const closeModalButton = recipeElement.querySelector('button');
+      closeModalButton.onclick = function () {
+        recipeElement.close();
+        document.body.removeChild(recipeElement);
+      };
+    }
+
+function handleCategoryChange(categoryNumber) {
+ // const selectedCategory = "Bolos";
+
+  //const selectedCategory = document.getElementById('categoria1').value;
+  const selectedCategory = document.getElementById(`categoria${categoryNumber}`).value;
+  console.log(selectedCategory);
+  const selectedRecipe = getRecipeByCategory(selectedCategory);
+console.log(selectedRecipe);
+  if (selectedRecipe) {
+    displayRecipeDetailsCategory(selectedRecipe);
+  }
+}
+
+function getRecipeByCategory(category =  "Massas") {
+  console.log("category", category);
+  // Obtenha a lista de receitas filtrada pela categoria
+  const filteredRecipes = recipesData.receitas.filter(recipe =>
+    recipe.categorias && recipe.categorias.includes(category)
+  );
+console.log("filteredRecipes",filteredRecipes);
+console.log("recipeData", recipesData.receitas);
+  // Retorne a primeira receita encontrada (ou null se não houver nenhuma)
+  return filteredRecipes.length > 0 ? filteredRecipes[0] : null;
+}
+
+
+function displayRecipeDetailsCategory(recipe) {
+  const recipeElement = document.createElement('dialog');
+  recipeElement.innerHTML = `<h2>${recipe.nome}</h2>
+                             <p>Chef: ${recipe.Nome_Chef}</p>
+                             <p>Ingredientes: ${recipe.ingredientes ? recipe.ingredientes.join(', ') : 'N/A'}</p>
+                             <p>Dificuldade: ${recipe.Nivel_de_dificuldade}</p>
+                             <p>Tempo de preparação: ${recipe.tempo_de_preparo}</p>
+                             <button onclick="closeModal()">Voltar</button>
+                             <hr>`;
+  document.body.appendChild(recipeElement);
+  recipeElement.showModal();
+
+  const closeModalButton = recipeElement.querySelector('button');
+  closeModalButton.onclick = function () {
+    recipeElement.close();
+    document.body.removeChild(recipeElement);
+  };
+}
+//Coloca o json no localStorage e pega as receitas do LocalStorage
+    document.addEventListener("DOMContentLoaded", function() {
+     
+  const existingRecipesString = localStorage.getItem('recipeData');
+  const existingRecipes = existingRecipesString ? JSON.parse(existingRecipesString) : { "receitas": [] };
+  const newRecipes = recipesData.receitas;
+
+  newRecipes.forEach(newRecipe => {
+    const recipeExists = existingRecipes.receitas.some(existingRecipe => existingRecipe.nome === newRecipe.nome);
+    if (!recipeExists) {
+      existingRecipes.receitas.push(newRecipe);
+    }
+  });
+
+ 
+  localStorage.setItem('recipeData', JSON.stringify(existingRecipes));
+  
+});
